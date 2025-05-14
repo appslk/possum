296121453de7da0a3f8c6737694db0dcc65d8271
Appslk <162972386+appslk@users.noreply.github.com>
2025-05-14 16:22:06 +0530

    Update CandyMint.tsx

--- src/components/CandyMint.tsx
+++ src/components/CandyMint.tsx
@@ -414,8 +414,6 @@ export const CandyMint: FC = () => {
 
     return (
         <div className="mintDetails">
-            <h1 className="possum-title">MINT POSSUM</h1>
-
             <div className="mint-info" id="colorH4">
                 <span id='txtColor'>Minted {_itemsRedeemed}/1000</span>
                 <div id='txtColor2'>Price: 0.75 SOL</div>
