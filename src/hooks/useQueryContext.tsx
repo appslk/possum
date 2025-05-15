2cdba82e2dd74fe2f339ca53d2881d9c3be0d491
Appslk <162972386+appslk@users.noreply.github.com>
2025-05-15 18:34:43 +0530

    Update useQueryContext.tsx

--- src/hooks/useQueryContext.tsx
+++ src/hooks/useQueryContext.tsx
@@ -5,8 +5,8 @@ export default function useQueryContext() {
   const router = useRouter()
   const { cluster } = router.query
 
-  const endpoint = cluster ? (cluster as EndpointTypes) : 'devnet'
-  const hasClusterOption = endpoint !== 'devnet'
+  const endpoint = cluster ? (cluster as EndpointTypes) : 'mainnet'
+  const hasClusterOption = endpoint !== 'mainnet'
   const fmtUrlWithCluster = (url) => {
     if (hasClusterOption) {
       const mark = url.includes('?') ? '&' : '?'
